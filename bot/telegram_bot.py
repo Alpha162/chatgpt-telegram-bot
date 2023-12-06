05fb6e474528c972abec5601f0625f52063cc567
DT <alpha162@gmail.com>
2023-12-06 20:46:31 +0000

    Update telegram_bot.py

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -285,6 +285,25 @@ class ChatGPTTelegramBot:
                 )
 
         await wrap_with_indicator(update, context, _generate, constants.ChatAction.UPLOAD_PHOTO)
+        
+  async def image(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """
+        Generates an image for the given prompt using DALL·E APIs
+        """
+        if not self.config['enable_image_generation'] \
+                or not await self.check_allowed_and_within_budget(update, context):
+            return
+
+        image_query = message_text(update.message)
+        if image_query == '':
+            await update.effective_message.reply_text(
+                message_thread_id=get_thread_id(update),
+                text=localized_text('image_no_prompt', self.config['bot_language'])
+            )
+            return
+
+        logging.info(f'New image generation request received from user {update.message.from_user.name} '
+                     f'(id: {update.message.from_user.id})')
 
     async def tts(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
